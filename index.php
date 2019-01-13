44d02825cb5845b0f46b1bc3789147326ee0c47c
Alfred <fred.jm.74@gmail.com>
2019-01-13 14:21:51 -0600

    Cambio de parametros publicacion

--- index.php
+++ index.php
@@ -1,28 +1,30 @@
+<?php
+
 /**
-  * Laravel – A PHP Framework For Web Artisans
-  *
-  * @package Laravel
-  * @author Taylor Otwell <taylorotwell@gmail.com>
-  */
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ * @author   Taylor Otwell <taylorotwell@gmail.com>
+ */
 
 /*
-|————————————————————————–
+|--------------------------------------------------------------------------
 | Register The Auto Loader
-|————————————————————————–
+|--------------------------------------------------------------------------
 |
 | Composer provides a convenient, automatically generated class loader for
-| our application. We just need to utilize it! We’ll simply require it
-| into the script here so that we don’t have to worry about manual
+| our application. We just need to utilize it! We'll simply require it
+| into the script here so that we don't have to worry about manual
 | loading any of our classes later on. It feels nice to relax.
 |
 */
 
-require __DIR__.’/bootstrap/autoload.php’;
+require __DIR__.'/bootstrap/autoload.php';
 
 /*
-|————————————————————————–
+|--------------------------------------------------------------------------
 | Turn On The Lights
-|————————————————————————–
+|--------------------------------------------------------------------------
 |
 | We need to illuminate PHP development, so let us turn on the lights.
 | This bootstraps the framework and gets it ready for use, then it
@@ -31,16 +33,16 @@ require __DIR__.’/bootstrap/autoload.php’;
 |
 */
 
-$app = require_once __DIR__.’/bootstrap/app.php’;
+$app = require_once __DIR__.'/bootstrap/app.php';
 
 /*
-|————————————————————————–
+|--------------------------------------------------------------------------
 | Run The Application
-|————————————————————————–
+|--------------------------------------------------------------------------
 |
 | Once we have the application, we can handle the incoming request
 | through the kernel, and send the associated response back to
-| the client’s browser allowing them to enjoy the creative
+| the client's browser allowing them to enjoy the creative
 | and wonderful application we have prepared for them.
 |
 */
@@ -48,7 +50,7 @@ $app = require_once __DIR__.’/bootstrap/app.php’;
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
-$request = Illuminate\Http\Request::capture()
+    $request = Illuminate\Http\Request::capture()
 );
 
 $response->send();
